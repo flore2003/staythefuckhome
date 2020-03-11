fbff2c2ac03c0cc243eef3f76b45c8526abfebaf
Florian Reifschneider <florian@rocketloop.de>
2020-03-11 19:29:47 +0100

    fix: Fixes issue with wrong timestamps in sitemap

--- filters/timestamp.js
+++ filters/timestamp.js
@@ -2,6 +2,6 @@
   A simple ISO timestamp for Nunjucks
 */
 module.exports = function (date) {
-  let timestamp = new Date()
-  return timestamp.getFullYear() + "-" + (timestamp.getMonth() + 1) + "-" + timestamp.getDate()
+  const timestamp = new Date(date)
+  return timestamp.toISOString();
 }
